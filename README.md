3c9443a458518ffe924791054e654c83d9d7b10a
Gregor <gregorzhilich@gmail.com>
2019-10-29 17:53:43 +0300

    Add README.md and main file

---
+++ README.md
@@ -0,0 +1,53 @@
+# **Math Module**
+This is a math module by Gregor Zhilich
+
+## **Using**
+
+### gcd
+math_module.gcd(1st_number, 2nd_number)
+
+### lcm
+math_module.lcm(1st_number, 2nd_number)
+
+### logarithm
+math_module.log(number, base)
+
+### factorial
+math_module.factorial(number)
+
+### power
+math_module.pow(number, power)
+
+### square root
+math_module.sqrt(number)
+
+### cube root
+math_module.cbrt(number)
+
+### absolute value
+math_module.abs(number)
+
+### sum of values from iterable
+math_module.iter_sum(iterable)
+
+### copy sign
+math_module.copysign(number, number_with_sign)
+
+### floor
+math_module.floor(float_value)
+
+### is infinite
+math_module.is_inf(number)
+
+### is NaN
+math_module.is_nan(number)
+
+### is finite
+math_module.isfinite(number)
+
+## **There is some constants**
+pi = 3.141592   
+tau = 6.283185  
+e = 2.718281   
+inf = infinity
+nan = NaN
---
+++ math_module.py
@@ -0,0 +1,90 @@
+pi = 3.141592   #pi number
+tau = 6.283185   #tau number
+e = 2.718281   #e number
+inf = float('inf')   #infinity
+nan = float('NaN')   #NaN
+
+def gcd(a,b):   #gcd function
+	'''Return the greatest common divisor of a and b'''
+	if a == 0:
+	       return b
+	res = gcd(b % a, a)
+	return res
+def lcm(a,b):   #lcm function
+	'''Return the least common divisor of a and b'''
+	res = (a * b) / gcd(a, b)
+	return int(res)
+def log(a, b):   #logarithm function
+	'''Return the logarithm of a with base b'''
+	res = 0
+	while a!=1:
+		a /= b
+		res += 1
+	return res
+def factorial(a):   #factorial function
+	'''Return the factorial of a'''
+	res = 1
+	for i in range(1, a+1):
+		res *= i
+	return res
+def pow(a, b):   #power function
+	'''Return a to the power of b'''
+	res = 1
+	for i in range(1, b+1):
+		res *= a
+	return res
+def sqrt(a):   #square function
+	'''Return the square root of a'''
+	res = a ** 0.5
+	if res.is_integer():
+		return int(res)
+	else:
+		return round(res, 3)
+def cbrt(a):   #cube root function
+	'''Return the cube root of a'''
+	res = a ** 0.33333
+	return round(res, 3)
+def abs(a):   #absolute value function
+	'''Return the absolute value of a'''
+	if a<0:
+		return -a
+	else:
+		return a
+def iter_sum(arr):   #function returns a sum of values in the iterable
+	'''Return the sum of values in the iterable'''
+	res = 0
+	for i in arr:
+		res += i
+	return int(round(res, 0))
+def copysign(a, b):   #function returns a with sign of b
+	'''Return a with sign of b'''
+	a = abs(a)
+	if b < 0:
+		res = -a
+	else:
+		res = a
+	return res
+def floor(a):
+	'''Return the floor of a as an Integral'''
+	res = int(round(a))
+	if res > a:
+		res-=1
+	return res
+def is_inf(a):   #test for infinity
+	'''Returns True if your value is infinity'''
+	if a != inf and a!=-inf:
+		return False
+	else:
+		return True
+def is_nan(a):   #test for NaN
+	'''Returns True if your value is NaN'''
+	if a != a:
+		return True
+	else:
+		return False
+def isfinite(a):
+    '''Return True if x is neither an infinity nor a NaN'''
+    if a==a and a!=inf and a!=-inf:
+        return True
+    else:
+        return False
